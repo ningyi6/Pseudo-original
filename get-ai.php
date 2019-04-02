c8b786f7de2502262d99bf3fa5b7e88475b1649c
不念 <admin@bunian.cn>
2019-04-02 08:38:59 +0800

    更新

--- get-ai.php
+++ get-ai.php
@@ -1,14 +1,18 @@
 <?php
 /*
-不念博客https://www.bunian.cn/ai/
-没有版权 任意分发
+*不念博客 
+*https://www.bunian.cn/
+*演示地址
+*https://www.bunian.cn/ai/
 */
-
 header('Access-Control-Allow-Origin:*');
 header('Content-type:text/html;charset=utf-8');
-function zh_en($text){
-	$entext = urlencode($text);
-	$url = 'http://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=zh-CN&tl=EN&q='.$entext;
+function translate($text,$translate=true){
+	if($translate){
+	$url = 'http://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=zh-CN&tl=EN&q='. urlencode($text);
+	}else{
+	$url = 'http://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=EN&tl=zh-CN&q='.urlencode($text);
+	}
 	set_time_limit(0);
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
@@ -30,30 +34,7 @@ function zh_en($text){
 	return implode(" ", $v);
 	}
 }
-function en_zh($text){
-	$entext = urlencode($text);
-	$url = 'http://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=EN&tl=zh-CN&q='.$entext;
-	set_time_limit(0);
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_MAXREDIRS,20);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 40);
-	curl_setopt($ch, CURLOPT_URL, $url);
-	$result = curl_exec($ch);
-	curl_close($ch);
-        $result = json_decode($result);
-	if(!empty($result)){
-	foreach($result[0] as $k){
-		$v[] = $k[0];
-	}
-	return implode(" ", $v);
-	}
-}
+/* 字符分割 */
 function mbStrSplit ($string, $len=1) {
   $start = 0;
   $strlen = mb_strlen($string);
@@ -66,13 +47,13 @@ function mbStrSplit ($string, $len=1) {
 }
 
 $infocount=mb_strlen($_POST['info'], 'UTF-8');
-if($_POST and $infocount<2000){
 
+
+if($_POST and $infocount<2000){
 $r = mbStrSplit($_POST['info'], 1000);
 $arr=count($r);
-
  for($i=0;$i<$arr;$i++){
-    echo en_zh(zh_en($r[$i]));
+    echo translate(translate($r[$i]),false);
  }
 //echo "\n 接口访问频繁。请24h后再试";
 }else{
