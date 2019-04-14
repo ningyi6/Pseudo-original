d0632198a4bed017bad376a5c9a552cf7d38b9ca
不念 <admin@bunian.cn>
2019-04-14 10:19:45 +0800

    去除无字数限制，为提高效率限字数为1000字
    去除无字数限制，为提高效率限字数为1000字

--- get-ai.php
+++ get-ai.php
@@ -7,12 +7,8 @@
 */
 header('Access-Control-Allow-Origin:*');
 header('Content-type:text/html;charset=utf-8');
-function translate($text,$translate=true){
-	if($translate){
-	$url = 'http://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=zh-CN&tl=EN&q='. urlencode($text);
-	}else{
-	$url = 'http://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=EN&tl=zh-CN&q='.urlencode($text);
-	}
+function translate($text,$from,$to){
+	$url = "http://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=$from&tl=$to&q=". urlencode($text);
 	set_time_limit(0);
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
@@ -34,28 +30,25 @@ function translate($text,$translate=true){
 	return implode(" ", $v);
 	}
 }
-/* 字符分割 */
-function mbStrSplit ($string, $len=1) {
-  $start = 0;
-  $strlen = mb_strlen($string);
-  while ($strlen) {
-    $array[] = mb_substr($string,$start,$len,"utf8");
-    $string = mb_substr($string, $len, $strlen,"utf8");
-    $strlen = mb_strlen($string);
-  }
-  return $array;
-}
-
-$infocount=mb_strlen($_POST['info'], 'UTF-8');
-
-
-if($_POST and $infocount<2000){
-$r = mbStrSplit($_POST['info'], 1000);
-$arr=count($r);
- for($i=0;$i<$arr;$i++){
-    echo translate(translate($r[$i]),false);
- }
-//echo "\n 接口访问频繁。请24h后再试";
-}else{
-echo '请输入需要伪原创的文章，并且文章内容不可以超过2000个字符';
+//去除无字数限制，为提高效率限字数为1000字
+//增加判断如为空，返回相应的文字
+if($_POST['info']){
+	$str = preg_replace('/[\x80-\xff]{1,3}/', ' ', $_POST['info'], -1); 
+	$num = strlen($str);
+	if($num<1030){
+		$zh_en=translate($_POST['info'],'zh-CN','EN');
+		if($zh_en){
+			$en_zh=translate($zh_en,'EN','zh-CN');
+			if($en_zh){
+				$info=$en_zh;
+			}else{
+			$info="超过IP限制，请稍后重试，或者添加qq群:200653131进行反馈";
+			}
+		}else{
+		$info="超过IP限制，请稍后重试，或者添加qq群:200653131进行反馈";
+		}
+	}else{
+	$info="您的字数超过1000文字，请删除一些文字在使用。";
+	}
+echo $info;
 }
